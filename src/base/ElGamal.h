d2bb8f69476801db9ccee10e2b9184d6f2569570
Qingyun Qiu <qiuqingyun98@outlook.com>
2021-04-13 13:36:45 +0800

    重构1

---
+++ src/base/ElGamal.h
@@ -0,0 +1,81 @@
+#ifndef ELGAMMAL_H_
+#define ELGAMMAL_H_
+
+#include <stdio.h>
+#include <time.h>
+#include <vector>
+#include <fstream>
+#include <NTL/ZZ.h>
+#include "Cipher_elg.h"
+#include "Mod_p.h"
+#include "G_q.h"
+NTL_CLIENT
+
+class ElGamal {
+private:
+	G_q G;  //Group used for encryption
+	ZZ sk;	//secret key
+	Mod_p pk; //public key
+public:
+	//Constructor & destructor
+	ElGamal();
+	ElGamal(long s, Mod_p p, G_q H);
+	ElGamal(ZZ s, Mod_p p, G_q H);
+	ElGamal(long s, G_q H);
+	ElGamal(ZZ s, G_q H);
+	ElGamal(Mod_p gen, long o, long mod, long s);
+	ElGamal(Mod_p gen, long o, ZZ mod, long s);
+	ElGamal(Mod_p gen, long o, ZZ mod, ZZ s);
+	ElGamal(Mod_p gen, ZZ o, ZZ mod, long s);
+	ElGamal(Mod_p gen, ZZ o, ZZ mod, ZZ s);
+	ElGamal(Mod_p gen, long o, long mod, long s, Mod_p p);
+	ElGamal(Mod_p gen, long o, ZZ mod, long s, Mod_p p);
+	ElGamal(Mod_p gen, long o, ZZ mod, ZZ s, Mod_p p);
+	ElGamal(Mod_p gen, ZZ o, ZZ mod, long s, Mod_p p);
+	ElGamal(Mod_p gen, ZZ o, ZZ mod, ZZ s, Mod_p p);
+	ElGamal(long o, long mod, long s, Mod_p p);
+	ElGamal(long o, ZZ mod, long s, Mod_p p);
+	ElGamal(ZZ o, ZZ mod, long s, Mod_p p);
+	ElGamal(long o, ZZ mod, ZZ s, Mod_p p);
+	ElGamal(ZZ o, ZZ mod, ZZ s, Mod_p p);
+	ElGamal(long o, long mod, long s);
+	ElGamal(long o, ZZ mod, long s);
+	ElGamal(ZZ o, ZZ mod, long s);
+	ElGamal(long o, ZZ mod, ZZ s);
+	ElGamal(ZZ o, ZZ mod, ZZ s);
+	virtual ~ElGamal();
+
+	//Access to the variables
+	G_q get_group() const;
+	Mod_p get_pk() const;
+	ZZ get_sk() const;
+
+	//functions to change parameters
+	void set_group(G_q G);
+	void set_sk(ZZ s);
+	void set_sk(long s);
+	void set_key(ZZ s, ZZ p);
+	void keyGen();
+
+	//encryption and decryption functions
+	Cipher_elg encrypt(Mod_p m);
+	Cipher_elg encrypt(ZZ m);
+	Cipher_elg encrypt(long m);
+
+	Cipher_elg encrypt(Mod_p m, long ran);
+	Cipher_elg encrypt(Mod_p m, ZZ ran);
+	Cipher_elg encrypt(ZZ m, long ran);
+	Cipher_elg encrypt(ZZ m, ZZ ran);
+	Cipher_elg encrypt(long m, long ran);
+	Cipher_elg encrypt(long m, ZZ ran);
+
+	//decryption function
+	Mod_p decrypt(Cipher_elg c);
+	ZZ decrypt(Cipher_elg c, int flag);
+
+	//Assigment operator
+	void operator =(const ElGamal& el);
+
+};
+
+#endif /* ELGAMMAL_H_ */
---
+++ src/shuffle/shuffle.h
@@ -0,0 +1,64 @@
+#pragma once
+#include "../base/Cipher_elg.h"
+#include "../base/ElGamal.h"
+#include "../base/G_q.h"
+#include "../base/global.h"
+#include "../base/sha256.h"
+#include "Functions.h"
+#include "Prover_toom.h"
+#include "Verifier_toom.h"
+
+extern G_q G;               // group used for the Pedersen commitment
+extern G_q H;              // group used for the the encryption
+extern ElGamal El;         // The class for encryption and decryption
+extern Pedersen Ped;        // Object which calculates the commitments
+
+class Shuffle {
+private:
+	ifstream ist;
+	ofstream ost;
+	vector<vector<Cipher_elg>*>* cipher_in;    //原始输入的密文
+	vector<vector<Cipher_elg>*>* cipher_out;   //重加密的密文
+	vector<vector<vector<int>*>*>* pi;        //Permutation，用于shuffle
+	vector<vector<ZZ>*>* R;			           //用于重加密的随机数
+	int mu = 4;                      // number of rows after reduction
+	int m_r = 7;                     // number of rows after reduction
+	int mu_h = 4;					  // 2*mu-1, number of extra elements in the reduction
+	int omega_mulex = 7;			  //windowsize for sliding-window technique
+	int omega_sw = 6;				  //windowsize for multi-expo technique
+	int omega_LL = 5;				  //windowsize for multi-expo technique
+	int m = 16;//行数
+	int n = 2;//列数
+	int ans = 0;//验证结果
+	ZZ mod;
+	ZZ ord;
+	ZZ gen;
+	ZZ genq;  // generator of Z_q，用于验证的生成元
+
+	//读取群的参数并生成群
+	void readParameters();
+	//设置ElGamal公私钥
+	void creatElGamal();
+	//读取文件中的密文，保存为16×2的矩阵形式
+	void readCipher(vector<vector<Cipher_elg>*>* Cipher);
+	//生成随机替换序列
+	void permutation(vector<int>* v, int N);
+	//生成随机替换矩阵
+	void perm_matrix(vector<vector<vector<int>* >* >* pi);
+	//生成随机数矩阵
+	void randomEl(vector<vector<ZZ>*>* R);
+	//重加密
+	void reencryptCipher();
+public:
+	//创建Prover角色
+	void creatProver();
+	//创建Verifier角色
+	void creatVerifier();
+	//进行shuffle操作
+	void shuffle();
+	//生成承诺
+	void prove();
+	//正确性验证
+	void verify();
+};
+
